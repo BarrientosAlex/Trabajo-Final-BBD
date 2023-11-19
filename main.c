2fb2d9df38fd6918542b619d66a041d904c528bb
LuchoDominella <luchodominella@gmail.com>
2023-11-19 13:49:33 -0300

    Validaciones Pacientes

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "menu.h"
+#include "ArbolPaciente.h"
+int main()
+{
+   char archivoPac[]= "pacientes.bin";
+    char archivoIng[]= "ingresos.bin";
+    char archivoPra[]= "practicas.bin";
 
-int main(){
-    menu();
-    return 0;
+    nodoPaciente * arbolPaciente = NULL;
+    arbolPaciente = arbolToarchi(arbolPaciente,archivoPac);
+    arbolPaciente = leerArchivoPaciente(archivoPac,arbolPaciente);
 }
